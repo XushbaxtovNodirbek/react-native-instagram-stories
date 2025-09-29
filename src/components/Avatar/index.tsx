862cea2ff5c8966044455f1e680082311434e447
XushbaxtovNodirbek <nodirbek.devoloper@gmail.com>
2025-09-29 14:57:12 +0500

    loader fixed

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,18 +1,19 @@
-import React, { FC, memo } from 'react';
-import {
-  View, Image, Text, TouchableOpacity,
-} from 'react-native';
+import React, { FC, memo } from "react";
+import { View, Image, Text, TouchableOpacity } from "react-native";
 import Animated, {
-  useSharedValue, useAnimatedStyle, useDerivedValue, withTiming,
-} from 'react-native-reanimated';
-import { StoryAvatarProps } from '../../core/dto/componentsDTO';
-import AvatarStyles from './Avatar.styles';
-import Loader from '../Loader';
-import { AVATAR_OFFSET } from '../../core/constants';
+  useSharedValue,
+  useAnimatedStyle,
+  useDerivedValue,
+  withTiming,
+} from "react-native-reanimated";
+import { StoryAvatarProps } from "../../core/dto/componentsDTO";
+import AvatarStyles from "./Avatar.styles";
+import Loader from "../Loader";
+import { AVATAR_OFFSET } from "../../core/constants";
 
-const AnimatedImage = Animated.createAnimatedComponent( Image );
+const AnimatedImage = Animated.createAnimatedComponent(Image);
 
-const StoryAvatar: FC<StoryAvatarProps> = ( {
+const StoryAvatar: FC<StoryAvatarProps> = ({
   id,
   avatarSource,
   name,
@@ -28,65 +29,72 @@ const StoryAvatar: FC<StoryAvatarProps> = ( {
   nameTextProps,
   renderAvatar,
   avatarBorderRadius,
-} ) => {
-
-  const loaded = useSharedValue( false );
-  const isLoading = useDerivedValue( () => loadingStory.value === id || !loaded.value );
-  const seen = useDerivedValue(
-    () => seenStories.value[id] === stories[stories.length - 1]?.id,
+}) => {
+  const loaded = useSharedValue(false);
+  const isLoading = useDerivedValue(
+    () => loadingStory.value === id || !loaded.value
   );
-  const loaderColor = useDerivedValue( () => ( seen.value ? seenColors : colors ) );
+  const seen = useDerivedValue(
+    () => seenStories.value[id] === stories[stories.length - 1]?.id
+  );
+  const loaderColor = useDerivedValue(() => (seen.value ? seenColors : colors));
 
   const onLoad = () => {
-
     loaded.value = true;
-
   };
 
-  const imageAnimatedStyles = useAnimatedStyle( () => (
-    { opacity: withTiming( isLoading.value ? 0.5 : 1 ) }
-  ) );
+  const imageAnimatedStyles = useAnimatedStyle(() => ({
+    opacity: withTiming(isLoading.value ? 0.5 : 1),
+  }));
 
-  if ( renderAvatar ) {
-
-    return renderAvatar( seen.value );
-
+  if (renderAvatar) {
+    return renderAvatar(seen.value);
   }
 
-  if ( !avatarSource ) {
-
+  if (!avatarSource) {
     return null;
-
   }
 
   return (
     <View style={AvatarStyles.name}>
       <View style={AvatarStyles.container}>
-        <TouchableOpacity activeOpacity={0.6} onPress={onPress} testID={`${id}StoryAvatar${stories.length}Story`}>
-          <Loader loading={isLoading} color={loaderColor} size={size + AVATAR_OFFSET * 2} />
+        <TouchableOpacity
+          activeOpacity={0.6}
+          onPress={onPress}
+          testID={`${id}StoryAvatar${stories.length}Story`}
+        >
+          <Loader
+            count={stories.length}
+            loading={isLoading}
+            color={loaderColor}
+            size={size + AVATAR_OFFSET * 2}
+          />
           <AnimatedImage
             source={avatarSource}
             style={[
               AvatarStyles.avatar,
               imageAnimatedStyles,
-              { width: size, height: size, borderRadius: avatarBorderRadius ?? ( size / 2 ) },
+              {
+                width: size,
+                height: size,
+                borderRadius: avatarBorderRadius ?? size / 2,
+              },
             ]}
             testID="storyAvatarImage"
             onLoad={onLoad}
           />
         </TouchableOpacity>
       </View>
-      {Boolean( showName ) && (
+      {Boolean(showName) && (
         <Text
           {...nameTextProps}
-          style={[ { width: size + AVATAR_OFFSET * 2 }, nameTextStyle ]}
+          style={[{ width: size + AVATAR_OFFSET * 2 }, nameTextStyle]}
         >
           {name}
         </Text>
       )}
     </View>
   );
-
 };
 
-export default memo( StoryAvatar );
+export default memo(StoryAvatar);
--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,113 +1,152 @@
-import React, {
-  FC, memo, useMemo, useState,
-} from 'react';
+import React, { FC, memo, useMemo, useState } from "react";
 import Animated, {
-  cancelAnimation, interpolate, runOnJS, useAnimatedProps, useAnimatedReaction, useAnimatedStyle,
-  useSharedValue, withRepeat, withTiming,
-} from 'react-native-reanimated';
+  cancelAnimation,
+  interpolate,
+  runOnJS,
+  useAnimatedProps,
+  useAnimatedReaction,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
+import { Circle, Defs, LinearGradient, Stop, Svg } from "react-native-svg";
 import {
-  Circle, Defs, LinearGradient, Stop, Svg,
-} from 'react-native-svg';
-import {
-  AVATAR_SIZE, LOADER_ID, LOADER_URL, STROKE_WIDTH,
-} from '../../core/constants';
-import { StoryLoaderProps } from '../../core/dto/componentsDTO';
+  AVATAR_SIZE,
+  LOADER_ID,
+  LOADER_URL,
+  STROKE_WIDTH,
+} from "../../core/constants";
+import { StoryLoaderProps } from "../../core/dto/componentsDTO";
 
-const AnimatedCircle = Animated.createAnimatedComponent( Circle );
-const AnimatedSvg = Animated.createAnimatedComponent( Svg );
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+const AnimatedSvg = Animated.createAnimatedComponent(Svg);
 
-const Loader: FC<StoryLoaderProps> = ( {
-  loading, color, size = AVATAR_SIZE + 10,
-} ) => {
+const Loader: FC<StoryLoaderProps> = ({
+  loading,
+  color,
+  size = AVATAR_SIZE + 10,
+  count = 1,
+}) => {
+  const RADIUS = useMemo(() => (size - STROKE_WIDTH) / 2, [size]);
+  const CIRCUMFERENCE = useMemo(() => RADIUS * 2 * Math.PI, [RADIUS]);
 
-  const RADIUS = useMemo( () => ( size - STROKE_WIDTH ) / 2, [ size ] );
-  const CIRCUMFERENCE = useMemo( () => RADIUS * 2 * Math.PI, [ RADIUS ] );
+  const [colors, setColors] = useState<string[]>(color.value);
 
-  const [ colors, setColors ] = useState<string[]>( color.value );
+  const rotation = useSharedValue(0);
+  const progress = useSharedValue(0);
 
-  const rotation = useSharedValue( 0 );
-  const progress = useSharedValue( 0 );
+  // Har bir segment uchun bo'shliq
+  const GAP_ANGLE = 10; // daraja hisobida bo'shliq
+  const SEGMENT_ANGLE = useMemo(
+    () => (360 - GAP_ANGLE * count) / count,
+    [count]
+  );
+  const SEGMENT_LENGTH = useMemo(
+    () => (CIRCUMFERENCE * SEGMENT_ANGLE) / 360,
+    [CIRCUMFERENCE, SEGMENT_ANGLE]
+  );
 
-  const animatedProps = useAnimatedProps( () => ( {
-    strokeDashoffset: interpolate( progress.value, [ 0, 1 ], [ 0, CIRCUMFERENCE * 2 / 3 ] ),
-  } ) );
-  const animatedStyles = useAnimatedStyle( () => ( {
-    transform: [ { rotate: `${rotation.value}deg` } ],
-  } ) );
+  const animatedProps = useAnimatedProps(() => ({
+    strokeDashoffset: interpolate(progress.value, [0, 1], [0, CIRCUMFERENCE]),
+  }));
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${rotation.value}deg` }],
+  }));
 
   const startAnimation = () => {
+    "worklet";
 
-    'worklet';
-
-    progress.value = withRepeat( withTiming( 1, { duration: 3000 } ), -1, true );
-    rotation.value = withRepeat( withTiming( 720, { duration: 3000 } ), -1, false, () => {
-
-      rotation.value = 0;
-
-    } );
-
+    rotation.value = withRepeat(withTiming(360, { duration: 1500 }), -1, false);
+    progress.value = withRepeat(withTiming(1, { duration: 3000 }), -1, true);
   };
 
   const stopAnimation = () => {
+    "worklet";
 
-    'worklet';
+    cancelAnimation(rotation);
+    rotation.value = withTiming(0);
 
-    cancelAnimation( progress );
-    progress.value = withTiming( 0 );
-
-    cancelAnimation( rotation );
-    rotation.value = withTiming( 0 );
-
+    cancelAnimation(progress);
+    progress.value = withTiming(0);
   };
 
-  const onColorChange = ( newColors: string[] ) => {
+  const onColorChange = (newColors: string[]) => {
+    "worklet";
 
-    'worklet';
-
-    if ( JSON.stringify( colors ) === JSON.stringify( newColors ) ) {
-
+    if (JSON.stringify(colors) === JSON.stringify(newColors)) {
       return;
-
     }
 
-    runOnJS( setColors )( newColors );
-
+    runOnJS(setColors)(newColors);
   };
 
   useAnimatedReaction(
     () => loading.value,
-    ( res ) => ( res ? startAnimation() : stopAnimation() ),
-    [ loading.value ],
+    (res) => (res ? startAnimation() : stopAnimation()),
+    [loading.value]
   );
+
   useAnimatedReaction(
     () => color.value,
-    ( res ) => onColorChange( res ),
-    [ color.value ],
+    (res) => onColorChange(res),
+    [color.value]
   );
+
+  // Har bir segment uchun dasharray va dashoffset hisoblash
+  const segments = useMemo(() => {
+    if (count < 3) {
+      return [
+        {
+          dashArray: [CIRCUMFERENCE],
+          dashOffset: 0,
+          rotation: -90,
+        },
+      ];
+    }
+
+    const result = [];
+    const gapLength = (CIRCUMFERENCE * GAP_ANGLE) / 360;
+
+    for (let i = 0; i < count; i++) {
+      const offset = i * (SEGMENT_LENGTH + gapLength);
+      result.push({
+        dashArray: [SEGMENT_LENGTH, CIRCUMFERENCE - SEGMENT_LENGTH],
+        dashOffset: -offset,
+        rotation: -90 + i * (SEGMENT_ANGLE + GAP_ANGLE),
+      });
+    }
+    return result;
+  }, [count, SEGMENT_LENGTH, CIRCUMFERENCE, SEGMENT_ANGLE, GAP_ANGLE]);
 
   return (
     <AnimatedSvg width={size} height={size} style={animatedStyles}>
       <Defs>
         <LinearGradient id={LOADER_ID} x1="0%" y1="0%" x2="100%" y2="0%">
-          {colors?.map( ( item, i ) => (
+          {colors?.map((item, i) => (
             <Stop key={item} offset={i / colors.length} stopColor={item} />
-          ) )}
+          ))}
         </LinearGradient>
       </Defs>
-      <AnimatedCircle
-        cx={size / 2}
-        cy={size / 2}
-        r={RADIUS}
-        fill="none"
-        stroke={LOADER_URL}
-        strokeWidth={STROKE_WIDTH}
-        strokeLinecap="round"
-        strokeDasharray={[ CIRCUMFERENCE ]}
-        animatedProps={animatedProps}
-      />
+      {segments.map((segment, index) => (
+        <AnimatedCircle
+          key={index}
+          cx={size / 2}
+          cy={size / 2}
+          r={RADIUS}
+          fill="none"
+          stroke={LOADER_URL}
+          strokeWidth={STROKE_WIDTH}
+          strokeLinecap="round"
+          strokeDasharray={segment.dashArray}
+          strokeDashoffset={segment.dashOffset}
+          transform={`rotate(${segment.rotation} ${size / 2} ${size / 2})`}
+          animatedProps={animatedProps}
+        />
+      ))}
     </AnimatedSvg>
   );
-
 };
 
-export default memo( Loader );
+export default memo(Loader);
